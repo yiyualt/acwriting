785b08381f0d5f56c17cb3dd9ecf09bb8a95278b
Yoki0 <1499114179@qq.com>
2020-08-07 18:09:11 +0900

    update

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
+
 setup(
     name= "acwrite",
-    version = "0.9",
+    version = "1.1",
     author="Yi Yu",
     author_email="q1499114179@gmail.com",
     description="A python package for writing academic papers",
@@ -20,5 +21,6 @@ setup(
     "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=["httpcore","httpx","nltk","goslate","bs4","requests"],
     scripts=['bin/acword']
     )
